cb2849475562a027083d09167780c31402b7acee
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2024-09-03 16:36:55 -0600

    Visual test mock input components

--- mortgage-calculator/src/App.js
+++ mortgage-calculator/src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import CalculatorInput from './components/CalculatorInput/CalculatorInput';
+import logo from "./logo.svg";
+import "./App.css";
+import CalculatorInput from "./components/CalculatorInput/CalculatorInput";
 
 function App() {
-  return (
-    <div className="App">
-      <CalculatorInput label="Mortgage Amount" designator="$"/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <CalculatorInput label="Mortgage Amount" designator="$" />
+            <CalculatorInput
+                label="Mortgage Term"
+                designator="years"
+                onLeft={false}
+            />
+            <CalculatorInput
+                label="Interest Rate"
+                designator="%"
+                onLeft={false}
+            />
+        </div>
+    );
 }
 
 export default App;
