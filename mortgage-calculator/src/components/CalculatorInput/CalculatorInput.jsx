bb6d2224e8c3c360718cb15892ad8fce52e86a14
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2024-09-04 10:37:59 -0600

    Finish number input styling.

--- mortgage-calculator/src/components/CalculatorInput/CalculatorInput.jsx
+++ mortgage-calculator/src/components/CalculatorInput/CalculatorInput.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import styles from './CalculatorInput.module.scss';
+import React from "react";
+import styles from "./CalculatorInput.module.scss";
 
-const CalculatorInput = ({label, designator, onLeft=true})=>{
-  return(
-    <div className={styles.container}>
-      <label htmlFor='container__input' className='container__label'>
-        {label}
-      </label>
-      {onLeft ? 
-      <div className={styles.mockInput}>
-        <p className='mock-input__designator'>{designator}</p>
-        <input className='mock-input__input' type='number'></input>
-      </div> : 
-      <div className={styles.mockInput}>
-        <input className='mock-input__input' type='number'></input>
-        <p className='mock-input__designator'>{designator}</p>
-      </div>}
-    </div>
-  );
-}
+const CalculatorInput = ({ label, designator, onLeft = true }) => {
+    return (
+        <div className={styles.container}>
+            <label htmlFor="container__input" className="container__label">
+                {label}
+            </label>
+            {onLeft ? (
+                <div className={styles.mockInput}>
+                    <p className="mock-input__designator">{designator}</p>
+                    <input className="mock-input__input" type="number"></input>
+                </div>
+            ) : (
+                <div className={styles.mockInput}>
+                    <input className="mock-input__input" type="number"></input>
+                    <p className="mock-input__designator">{designator}</p>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default CalculatorInput;
